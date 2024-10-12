23d7e240b8743a24261b21272114c966a0414a14
Heverton <hb.hevertonborges@gmail.com>
2024-10-12 17:47:46 -0300

    rewritten all

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,22 @@
-import { createPerson, getPersonById, getPersonByQuery } from "./controllers";
-
 const server = Bun.serve({
   async fetch(req) {
-    const { pathname } = new URL(req.url)
+    const url = new URL(req.url);
 
-    if (req.method == "POST" && pathname == "/pessoas") {
-      const person = req.body
-      const personId = await createPerson(person)
-      return new Response("OK")
+    if (url.pathname === "/pessoas" && req.method === "POST") {
+      return new Response("Ok")
     }
-
-    else if (req.method == "GET" && pathname.startsWith("/pessoas/")) {
-      const id = pathname.replace("/pessoas/", "")
-      const person = await getPersonById(id)
-      return Response.json(person)
+    if (url.pathname === "/pessoas/" && req.method === "GET") {
+      return new Response("Ok")
     }
-
-    else if (req.method == "GET" && pathname.startsWith("/pessoas")) {
-      const initTerm = req.url.indexOf("?")
-      const term = req.url.slice(initTerm + 3)
-      const people = await getPersonByQuery(term)
-      return Response.json(people)
+    if (url.pathname === "/pessoas?" && req.method === "GET") {
+      return new Response("Ok")
     }
-
-    else if (req.method == "GET" && pathname == "/contagem-pessoas") {
-      return new Response("contagem-pessoas")
+    if (url.pathname === "/contagem-pessoas" && req.method === "GET") {
+      return new Response("Ok")
     }
 
     return new Response("Path not found", { status: 404 })
   },
-});
+})
 
-console.log(`Listening on ${server.url.origin} ...`);
+console.log(`Server running on ${server.url.origin}`)
